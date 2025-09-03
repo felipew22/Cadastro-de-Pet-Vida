90ead054f9ed174a998a03e118ae156f24d98c4b
felipew22 <Lf0349522@gmail.com>
2025-09-03 18:20:04 -0300

    CRUD Agendar Consulta

--- src/DAO/AgendarConsultaDAO.java
+++ src/DAO/AgendarConsultaDAO.java
@@ -1,4 +1,132 @@
 package DAO;
 
+import connectionDataBase.IniciarDB;
+import models.AgendarConsulta;
+
+import javax.swing.*;
+import java.sql.*;
+import java.util.ArrayList;
+
 public class AgendarConsultaDAO {
+
+
+    //Inserir
+    public void insert (AgendarConsulta agendarConsulta){
+        String sql = "INSERT INTO agendar_consulta(data_hora,id_tutor,id_veterinario,id_animal) VALUES (?,?,?,?)";
+
+        System.out.println(sql);
+
+        try {
+            Connection connection = IniciarDB.obterConexao();
+            PreparedStatement ps = connection.prepareStatement(sql);
+            ps.setTimestamp(1, new Timestamp(agendarConsulta.getDatahora().getTime()));
+            ps.setInt(2, agendarConsulta.getTutor());
+            ps.setInt(3,agendarConsulta.getVeterinario());
+            ps.setInt(4,agendarConsulta.getAnimal());
+
+            ps.execute();
+            JOptionPane.showMessageDialog(null, "Cadastro realizado com Sucesso!");
+            ps.close();
+            connection.close();
+        } catch (SQLException e) {
+            JOptionPane.showMessageDialog(null, "Erro no cadastro!");
+            e.printStackTrace();
+        }
+    }
+
+    //Atualizar
+    public void update(AgendarConsulta agendarConsulta){
+        String sql = "UPDATE agendar_consulta SET data_hora=? ,id_tutor=? ,id_veterinario=? ,id_animal=? WHERE id=?";
+        System.out.println(sql);
+
+        try {
+            Connection connection = IniciarDB.obterConexao();
+            PreparedStatement ps = connection.prepareStatement(sql);
+            ps.setTimestamp(1, new Timestamp(agendarConsulta.getDatahora().getTime()));
+            ps.setInt(2, agendarConsulta.getTutor());
+            ps.setInt(3,agendarConsulta.getVeterinario());
+            ps.setInt(4,agendarConsulta.getAnimal());
+
+            ps.execute();
+            JOptionPane.showMessageDialog(null, "Alteração realizado com Sucesso!");
+            ps.close();
+            connection.close();
+    } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    //Deletar
+    public void delete(AgendarConsulta agendarConsulta) {
+        String sql =   "DELETE FROM agendar_consulta WHERE id=?";
+        System.out.println(sql);
+        try {
+            Connection connection = IniciarDB.obterConexao();
+            PreparedStatement ps = connection.prepareStatement(sql);
+            ps.setInt(1, agendarConsulta.getId());
+            ps.execute();
+            JOptionPane.showMessageDialog(null, "Exclusão realizada com sucesso");
+            ps.close();
+            connection.close();
+
+        } catch (SQLException e) {
+            JOptionPane.showMessageDialog(null, "Usuario não excluido");
+            e.printStackTrace();
+        }
+    }
+
+    // Consultar
+    public void consulta(AgendarConsulta agendarConsulta) {
+        String sql = "select a.data_hora, b.nome, d.nome, e.nome_pet from agendar_consulta a, tutor b, veterinario c, funcionarios d, animal e where a.id=?";
+
+        System.out.println(sql);
+        try {
+            Connection connection = IniciarDB.obterConexao();
+            PreparedStatement ps = connection.prepareStatement(sql);
+            ps.setInt(1, agendarConsulta.getId());
+            ResultSet rs = ps.executeQuery();
+            agendarConsulta.setExiste(false);
+            while (rs.next()){
+                agendarConsulta.setExiste(true);
+                agendarConsulta.setDatahora(rs.getTimestamp(1));
+                agendarConsulta.setTutorname(rs.getString(2));
+                agendarConsulta.setVeterinarioname(rs.getString(3));
+                agendarConsulta.setAnimalname(rs.getString(4));
+
+            }
+        }
+        catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+
+    public ArrayList listAgendamentos(){
+        ArrayList<AgendarConsulta> listaAgendar = new ArrayList<>();
+
+        String sql = "select a.data_hora, b.nome, d.nome, e.nome_pet,a.id from agendar_consulta a, tutor b, veterinario c, funcionarios d, animal e ";
+
+        try{
+            Connection connection = IniciarDB.obterConexao();
+            PreparedStatement ps = connection.prepareStatement(sql);
+
+            ResultSet resultSet = ps.executeQuery();
+
+            while (resultSet.next()){
+                AgendarConsulta agendarConsulta = new AgendarConsulta();
+
+                agendarConsulta.setDatahora(resultSet.getTimestamp(1));
+                agendarConsulta.setTutorname(resultSet.getString(2));
+                agendarConsulta.setVeterinarioname(resultSet.getString(3));
+                agendarConsulta.setAnimalname(resultSet.getString(4));
+                agendarConsulta.setId(5);
+
+                listaAgendar.add(agendarConsulta);
+            }
+
+        } catch (Exception e) {
+            JOptionPane.showMessageDialog(null, e);
+        }
+        return listaAgendar;
+    }
+
 }
--- src/models/AgendarConsulta.java
+++ src/models/AgendarConsulta.java
@@ -1,6 +1,103 @@
 package models;
 
+
+import java.util.Date;
+
 public class AgendarConsulta {
-    private String data_hora;
-    private S
+    private Date datahora;
+    private int tutor;
+    private int veterinario;
+    private int animal;
+    private int id;
+
+    private String datahorastr;
+    private String tutorname;
+    private String veterinarioname;
+    private String animalname;
+    private boolean existe;
+
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+
+
+    public Date getDatahora() {
+        return datahora;
+    }
+
+    public void setDatahora(Date datahora) {
+        this.datahora = datahora;
+    }
+
+    public int getTutor() {
+        return tutor;
+    }
+
+    public void setTutor(int tutor) {
+        this.tutor = tutor;
+    }
+
+    public int getVeterinario() {
+        return veterinario;
+    }
+
+    public void setVeterinario(int veterinario) {
+        this.veterinario = veterinario;
+    }
+
+    public int getAnimal() {
+        return animal;
+    }
+
+    public void setAnimal(int animal) {
+        this.animal = animal;
+    }
+
+    public String getDatahorastr() {
+        return datahorastr;
+    }
+
+    public void setDatahorastr(String datahorastr) {
+        this.datahorastr = datahorastr;
+    }
+
+    public String getTutorname() {
+        return tutorname;
+    }
+
+    public void setTutorname(String tutorname) {
+        this.tutorname = tutorname;
+    }
+
+    public String getVeterinarioname() {
+        return veterinarioname;
+    }
+
+    public void setVeterinarioname(String veterinarioname) {
+        this.veterinarioname = veterinarioname;
+    }
+
+    public String getAnimalname() {
+        return animalname;
+    }
+
+    public void setAnimalname(String animalname) {
+        this.animalname = animalname;
+    }
+
+    public boolean isExiste() {
+        return existe;
+    }
+
+    public void setExiste(boolean existe) {
+        this.existe = existe;
+    }
+
+
 }
